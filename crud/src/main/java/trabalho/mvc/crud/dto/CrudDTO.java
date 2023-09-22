02330681e1f6dd3674b3fb2a8a2ffb098ccedcc9
Gabriel <131317539+KnowledgeMoor@users.noreply.github.com>
2023-09-22 15:33:45 -0300

    Add files via upload

---
+++ crud/src/main/java/trabalho/mvc/crud/dto/CrudDTO.java
@@ -0,0 +1,13 @@
+package trabalho.mvc.crud.dto;
+
+import lombok.Data;
+
+@Data
+public class CrudDTO {
+    
+    private String name;
+    private String address;
+    private String city;
+    private String country;
+    private int Pin_code;
+}
---
+++ crud/src/main/java/trabalho/mvc/crud/resources/CrudResources.java
@@ -0,0 +1,47 @@
+package trabalho.mvc.crud.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import trabalho.mvc.crud.dto.CrudDTO;
+
+@Controller
+public class CrudResources {
+
+    public List<CrudDTO> cadastros = new ArrayList<>();
+
+    @PostMapping("CrudPost")
+    public String doPost(CrudDTO dto, Model model) {
+        cadastros.add(dto);
+        return doGet(model);
+    }
+
+    @RequestMapping("CrudGet")
+    public String doGet(Model model) {
+        model.addAttribute("cadastros", cadastros);
+        return "lista";
+    }
+
+    @GetMapping("/delete/{index}")
+    public String deleteRecord(@PathVariable int index) {
+        if (index >= 0 && index < cadastros.size()) {
+            cadastros.remove(index);
+        }
+        return "redirect:/CrudGet";
+    }
+
+    @GetMapping("/editPost/{index}")
+    public String doEdit(int index, CrudDTO dto) {
+
+        cadastros.set(index, dto);
+        return "redirect:/CrudGet";
+    }
+
+}
